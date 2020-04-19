5580bbf3bc894cbba20708c8217cfcd676c3617a
Thermoflux <Thermoflux@users.noreply.github.com>
2020-04-19 18:31:34 +0530

    Working on Arch zsh

--- sysMetrics.sh
+++ sysMetrics.sh
@@ -1,16 +1,16 @@
 #! /bin/bash
 
-## Used contents from blog post at http://www.systeen.com/2016/05/07/bash-script-monitor-cpu-memory-disk-usage-linux/ 
+## Used contents from blog post at http://www.systeen.com/2016/05/07/bash-script-monitor-cpu-memory-disk-usage-linux/
 ## to build the following function
 
 function dumpSysMetrics(){
 MEMORY=$(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }')
 DISK=$(df -h | awk '$NF=="/"{printf "%s\t\t", $5}')
 CPU=$(top -bn1 | grep load | awk '{printf "%.2f%%\t\t\n", $(NF-2)}')
-now = date
-echo "$now, $MEMORY$DISK$CPU" >> $1
+nowtime=$(date)
+echo "$nowtime, Memory:$MEMORY Disk:$DISK CPU:$CPU" >> $1
 }
-## If you need to run this through cron, uncomment line 11. 
+## If you need to run this through cron, uncomment line 11.
 ## Or else you can use the command line to call this function
 ## Call the above function here with log file path as an input
-# dumpSysMetrics('~/Logs/sysMetrics.log')
+# dumpSysMetrics '~/Logs/sysMetrics.log'
