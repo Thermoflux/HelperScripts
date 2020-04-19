818bc667b5a8a42fa87594df0699615de7a8d1aa
Thermoflux <Thermoflux@users.noreply.github.com>
2020-04-19 18:21:52 +0530

    Added Date and corrected inputs

--- sysMetrics.sh
+++ sysMetrics.sh
@@ -3,11 +3,12 @@
 ## Used contents from blog post at http://www.systeen.com/2016/05/07/bash-script-monitor-cpu-memory-disk-usage-linux/ 
 ## to build the following function
 
-function dumpSysMetrics($LogFilePath){
+function dumpSysMetrics(){
 MEMORY=$(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }')
 DISK=$(df -h | awk '$NF=="/"{printf "%s\t\t", $5}')
 CPU=$(top -bn1 | grep load | awk '{printf "%.2f%%\t\t\n", $(NF-2)}')
-echo "$MEMORY$DISK$CPU" > $LogFilePath
+now = date
+echo "$now, $MEMORY$DISK$CPU" >> $1
 }
 ## If you need to run this through cron, uncomment line 11. 
 ## Or else you can use the command line to call this function
