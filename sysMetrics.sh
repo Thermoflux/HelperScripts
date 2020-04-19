bc13f39b59743c6d1173cc2178d7169ec371acf1
Thermoflux <Thermoflux@users.noreply.github.com>
2020-04-19 23:04:36 +0530

    if no inputs, will print to std out

--- sysMetrics.sh
+++ sysMetrics.sh
@@ -8,7 +8,13 @@ MEMORY=$(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }')
 DISK=$(df -h | awk '$NF=="/"{printf "%s\t\t", $5}')
 CPU=$(top -bn1 | grep load | awk '{printf "%.2f%%\t\t\n", $(NF-2)}')
 nowtime=$(date)
-echo "$nowtime, Memory:$MEMORY Disk:$DISK CPU:$CPU" >> $1
+if [ $# -eq 0 ]; then
+    echo "$nowtime, Memory:$MEMORY Disk:$DISK CPU:$CPU"
+    exit 1
+else
+	echo "$nowtime, Memory:$MEMORY Disk:$DISK CPU:$CPU" >> $1
+	exit 1
+fi
 }
 ## If you need to run this through cron, uncomment line 11.
 ## Or else you can use the command line to call this function
